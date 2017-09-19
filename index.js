b173d1ee543f9f401f115c3d40d77fd6caa0292d
Fabio Pagoti <fabio@duxinnovation.com>
2017-09-18 21:04:42 -0300

    resolvido

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 // Gerar a aplicação
 const app = express();
 
 // Configurar o handlebars
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 //Middleware
@@ -15,11 +18,40 @@ app.use(express.static('assets'));
 app.use(bodyParser.urlencoded());
 
 //Rotas
-app.get('/', (request, response) => {
-    response.render('index');
-});
+app.get('/', (req, res) => {
+
+	request.get('http://api.fixer.io/latest?base=brl', (error, apiRequest, body) => {
+		let data = JSON.parse(body);
+		res.render('index', {
+			taxa: data.rates.USD
+		});
+	})
+
+})
+
+app.post('/conversao', (req, res) => {
+
+
+	request.get('http://api.fixer.io/latest?base=brl', (error, apiRequest, body) => {
+		let data = JSON.parse(body);
+		let valor = req.body.valor;
+		let cotacao = data.rates.USD;
+		let resultado = valor * cotacao;
+
+		// res.redirect('/resposta?resultado=' + resultado);
+
+		res.render('resposta', {
+			valor: Number(resultado).toFixed(2)
+		})
+
+	})
+})
+
+app.get('/conversao', (req, res) => {
+	res.redirect('/')
+})
 
 //Inicializar o servidor
 app.listen(3000, () => {
-    console.log('Servidor inicializado');
+	console.log('Servidor inicializado');
 });
